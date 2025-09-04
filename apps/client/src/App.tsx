a021e36d4db7e26d9586a71a421003bef513a7e8
HitsujiRere <hitsujirere@gmail.com>
2025-09-04 13:23:07 +0900

    feat(client): TailwindCSS，shadecn/uiの導入

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,9 +8,9 @@ function App() {
     <>
       <h1>LoL Team Balancer v2</h1>
       <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
       </div>
     </>
   );
