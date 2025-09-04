f2474b74cb4e46b0db64500ab6f1f163faaec06a
HitsujiRere <hitsujirere@gmail.com>
2025-09-04 13:05:07 +0900

    feat(build): clientのtsconfigの切り出し

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion:"../index.html" has a root element.
 createRoot(document.getElementById("root")!).render(
