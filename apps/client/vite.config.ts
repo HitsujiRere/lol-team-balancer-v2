73fdc69429a127d054e2c465f25dd42c074a2a6d
HitsujiRere <hitsujirere@gmail.com>
2025-09-05 16:01:14 +0900

    feat(client): Vercelへのデプロイ

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,9 +1,29 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "./logs/stats.html",
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom/client"],
+        },
+      },
+    },
+  },
 });
