2c05850ec19d16e38688104e334c9054c53ee39c
HitsujiRere <hitsujirere@gmail.com>
2025-12-05 11:08:48 +0900

    feat: ロビーチャットの変更実装

--- src/features/TeamBalancer/ChatInput/index.tsx
+++ src/features/TeamBalancer/ChatInput/index.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { XIcon } from "lucide-react";
+import { useId, useState } from "react";
+import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
@@ -6,10 +11,32 @@ export const ChatInput = ({
   className,
   ...props
 }: React.ComponentProps<"div">) => {
+  const textareaId = useId();
+
+  const [chat, setChat] = useState("");
+  const changeChat = (chat: string) => {
+    setChat(chat);
+  };
+
   return (
     <div className={cn("flex flex-col gap-1", className)} {...props}>
-      <Label>ロビーチャット</Label>
-      <Textarea placeholder="サモナー #JP1がロビーに参加しました。" />
+      <Label htmlFor={textareaId}>ロビーチャット</Label>
+      <div className="relative">
+        <Textarea
+          id={textareaId}
+          placeholder="サモナー #JP1がロビーに参加しました。"
+          value={chat}
+          onChange={(event) => changeChat(event.target.value)}
+        />
+        <Button
+          variant="ghost"
+          className="absolute top-2 right-2"
+          disabled={chat === ""}
+          onClick={() => changeChat("")}
+        >
+          <XIcon />
+        </Button>
+      </div>
     </div>
   );
 };
